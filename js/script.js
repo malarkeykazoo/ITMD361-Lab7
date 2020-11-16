e57acce976d533484e714768f752761cf741dbb3
Josh <6891850+malarkeykazoo@users.noreply.github.com>
2020-11-16 10:34:31 -0600

    Update script.js
    Pulling name via JS vs hard coding.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ function init(){
 //add your javascrip between these two lines of code
   document.getElementById("entrybutton").addEventListener("click", f1);
   function f1() {
+    var hObj =  document.querySelectorAll("h2.center");
+    var hText = hObj.item(0).innerHTML;
     var ei = document.getElementById("entryinput").value;
     document.getElementById("textoutput").innerHTML = ei;
-    alert("Joshua Lau: " + ei);
+    alert(hText + ": " + ei);
   }
   window.addEventListener('load', init);
 }
