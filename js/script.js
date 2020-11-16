5defae0fbf6316f7874a683272d429353b009cff
Josh <6891850+malarkeykazoo@users.noreply.github.com>
2020-11-16 10:21:44 -0600

    add JS and call from html
    Add JS code to take input and alert/rewrite html. Added html to initialize JS.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
-window.addEventListener('load', init);
+  document.getElementById("entrybutton").addEventListener("click", f1);
+  function f1() {
+    var ei = document.getElementById("entryinput").value;
+    document.getElementById("textoutput").innerHTML = ei;
+    alert("Joshua Lau: " + ei);
+  }
+  window.addEventListener('load', init);
+}
